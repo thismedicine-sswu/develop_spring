7a4d9c2d7816905e8c599151497fdbdef7d4fa3b
rlaalswn222 <rlaalswn0202@naver.com>
2024-05-09 13:35:12 +0900

    spring develop

---
+++ src/main/java/LeeYak/LeeYak/LeeYakApplication.java
@@ -0,0 +1,13 @@
+package LeeYak.LeeYak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeYakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeYakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LeeYak/LeeYak/LeeYakApplicationTests.java
@@ -0,0 +1,13 @@
+package LeeYak.LeeYak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeYakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
